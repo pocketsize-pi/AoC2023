4cc86ea08bbbc0a1b71210a9b29200900036f65c
Cristina Rodriguez <cristina.rodriguez.t@gmail.com>
2023-11-28 15:28:42 +0000

    Parameterise call to cargo run

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
-use AoC2023::*;
+use std::env;
+
 pub mod day01;
 
 fn main() {
     println!("Hello, Advent of Code!");
 
-    // Day 1
-    day01::day01();
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() == 0 {
+        // No choice
+        println!("Try again with a day")
+    }
+    else {
 
-    // Day 2
+        if args[0].parse::<u8>().is_ok() {
+            let day = args[0].parse::<u8>().unwrap();
 
-
-    // etc
+            match day {
+                1 => day01::day01(),
+                _others => println!("This day doesn't exist yet")}
+        }
+        else {
+            println!("Not a number, try again")
+        }
+    }
 
 }
