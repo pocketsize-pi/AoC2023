b7f6241b9456c09c19cf02381df2825534454569
Cristina Rodriguez <cristina.rodriguez.t@gmail.com>
2023-12-18 11:31:05 +0000

    Day 16 part 2

--- src/day16.rs
+++ src/day16.rs
@@ -203,6 +203,379 @@ pub fn day16(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
     // 5297 too low
     // 7996 is right
 
+    // Part 2 brute force copy paste
+    let mut configuration_energised = Vec::new();
+
+    for start_r in 0..num_rows {
+        let mut energised_vec = Vec::new();
+        let mut beam_list = VecDeque::new();
+        let mut beam_list_done = VecDeque::new();
+        let mut visited_nodes = Vec::new();
+        let start_point = Point {r: start_r as i32, c:-1};
+        let start_dir = East;
+
+        beam_list.push_back((start_point.clone(), start_dir.clone()));
+
+        while !beam_list.is_empty() {
+            let current_light = beam_list.pop_front().unwrap();
+            let mut light_position = current_light.0;
+            let mut light_direction = current_light.1;
+
+            while !light_out(&move_point(&light_position, light_direction), num_rows, num_cols) &&
+                !visited_nodes.contains(&(move_point(&light_position, light_direction), light_direction)) {
+                let new_point = move_point(&light_position, light_direction);
+                visited_nodes.push((new_point, light_direction));
+                let new_entry = contraption[(new_point.r as usize, new_point.c as usize)];
+                if new_entry == '.' {
+                    // good, continue along
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                } else if new_entry == '|' {
+                    if (light_direction == North) | (light_direction == South) {
+                        light_position = new_point;
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    } else {
+                        light_position = new_point;
+                        light_direction = North;
+                        beam_list.push_back((new_point, South));
+                        beam_list.push_back((new_point, South));
+                        beam_list_done.push_back((new_point, South));
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    }
+                } else if new_entry == '-' {
+                    if (light_direction == East) | (light_direction == West) {
+                        light_position = new_point;
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    } else {
+                        light_position = new_point;
+                        light_direction = East;
+                        beam_list.push_back((new_point, West));
+                        beam_list_done.push_back((new_point, West));
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    }
+                } else if new_entry == '\\' {
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                    if light_direction == North {
+                        light_direction = West;
+                    } else if light_direction == East {
+                        light_direction = South;
+                    } else if light_direction == South {
+                        light_direction = East;
+                    } else if light_direction == West {
+                        light_direction = North;
+                    }
+                } else if new_entry == '/' {
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                    if light_direction == North {
+                        light_direction = East;
+                    } else if light_direction == East {
+                        light_direction = North;
+                    } else if light_direction == South {
+                        light_direction = West;
+                    } else if light_direction == West {
+                        light_direction = South;
+                    }
+                }
+            }
+        }
+        configuration_energised.push(energised_vec.len());
+    }
+
+    for start_r in (0..num_rows).rev() {
+        let mut energised_vec = Vec::new();
+        let mut beam_list = VecDeque::new();
+        let mut beam_list_done = VecDeque::new();
+        let mut visited_nodes = Vec::new();
+        let start_point = Point {r: start_r as i32, c:num_cols as i32};
+        let start_dir = West;
+
+        beam_list.push_back((start_point.clone(), start_dir.clone()));
+
+        while !beam_list.is_empty() {
+            let current_light = beam_list.pop_front().unwrap();
+            let mut light_position = current_light.0;
+            let mut light_direction = current_light.1;
+
+            while !light_out(&move_point(&light_position, light_direction), num_rows, num_cols) &&
+                !visited_nodes.contains(&(move_point(&light_position, light_direction), light_direction)) {
+                let new_point = move_point(&light_position, light_direction);
+                visited_nodes.push((new_point, light_direction));
+                let new_entry = contraption[(new_point.r as usize, new_point.c as usize)];
+                if new_entry == '.' {
+                    // good, continue along
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                } else if new_entry == '|' {
+                    if (light_direction == North) | (light_direction == South) {
+                        light_position = new_point;
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    } else {
+                        light_position = new_point;
+                        light_direction = North;
+                        beam_list.push_back((new_point, South));
+                        beam_list.push_back((new_point, South));
+                        beam_list_done.push_back((new_point, South));
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    }
+                } else if new_entry == '-' {
+                    if (light_direction == East) | (light_direction == West) {
+                        light_position = new_point;
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    } else {
+                        light_position = new_point;
+                        light_direction = East;
+                        beam_list.push_back((new_point, West));
+                        beam_list_done.push_back((new_point, West));
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    }
+                } else if new_entry == '\\' {
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                    if light_direction == North {
+                        light_direction = West;
+                    } else if light_direction == East {
+                        light_direction = South;
+                    } else if light_direction == South {
+                        light_direction = East;
+                    } else if light_direction == West {
+                        light_direction = North;
+                    }
+                } else if new_entry == '/' {
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                    if light_direction == North {
+                        light_direction = East;
+                    } else if light_direction == East {
+                        light_direction = North;
+                    } else if light_direction == South {
+                        light_direction = West;
+                    } else if light_direction == West {
+                        light_direction = South;
+                    }
+                }
+            }
+        }
+        configuration_energised.push(energised_vec.len());
+    }
+
+    for start_c in 0..num_cols {
+        let mut energised_vec = Vec::new();
+        let mut beam_list = VecDeque::new();
+        let mut beam_list_done = VecDeque::new();
+        let mut visited_nodes = Vec::new();
+        let start_point = Point {c: start_c as i32, r:-1};
+        let start_dir = South;
+
+        beam_list.push_back((start_point.clone(), start_dir.clone()));
+
+        while !beam_list.is_empty() {
+            let current_light = beam_list.pop_front().unwrap();
+            let mut light_position = current_light.0;
+            let mut light_direction = current_light.1;
+
+            while !light_out(&move_point(&light_position, light_direction), num_rows, num_cols) &&
+                !visited_nodes.contains(&(move_point(&light_position, light_direction), light_direction)) {
+                let new_point = move_point(&light_position, light_direction);
+                visited_nodes.push((new_point, light_direction));
+                let new_entry = contraption[(new_point.r as usize, new_point.c as usize)];
+                if new_entry == '.' {
+                    // good, continue along
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                } else if new_entry == '|' {
+                    if (light_direction == North) | (light_direction == South) {
+                        light_position = new_point;
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    } else {
+                        light_position = new_point;
+                        light_direction = North;
+                        beam_list.push_back((new_point, South));
+                        beam_list.push_back((new_point, South));
+                        beam_list_done.push_back((new_point, South));
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    }
+                } else if new_entry == '-' {
+                    if (light_direction == East) | (light_direction == West) {
+                        light_position = new_point;
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    } else {
+                        light_position = new_point;
+                        light_direction = East;
+                        beam_list.push_back((new_point, West));
+                        beam_list_done.push_back((new_point, West));
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    }
+                } else if new_entry == '\\' {
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                    if light_direction == North {
+                        light_direction = West;
+                    } else if light_direction == East {
+                        light_direction = South;
+                    } else if light_direction == South {
+                        light_direction = East;
+                    } else if light_direction == West {
+                        light_direction = North;
+                    }
+                } else if new_entry == '/' {
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                    if light_direction == North {
+                        light_direction = East;
+                    } else if light_direction == East {
+                        light_direction = North;
+                    } else if light_direction == South {
+                        light_direction = West;
+                    } else if light_direction == West {
+                        light_direction = South;
+                    }
+                }
+            }
+        }
+        configuration_energised.push(energised_vec.len());
+    }
+
+    for start_c in (0..num_cols).rev() {
+        let mut energised_vec = Vec::new();
+        let mut beam_list = VecDeque::new();
+        let mut beam_list_done = VecDeque::new();
+        let mut visited_nodes = Vec::new();
+        let start_point = Point {c: start_c as i32, r:num_rows as i32};
+        let start_dir = North;
+
+        beam_list.push_back((start_point.clone(), start_dir.clone()));
+
+        while !beam_list.is_empty() {
+            let current_light = beam_list.pop_front().unwrap();
+            let mut light_position = current_light.0;
+            let mut light_direction = current_light.1;
+
+            while !light_out(&move_point(&light_position, light_direction), num_rows, num_cols) &&
+                !visited_nodes.contains(&(move_point(&light_position, light_direction), light_direction)) {
+                let new_point = move_point(&light_position, light_direction);
+                visited_nodes.push((new_point, light_direction));
+                let new_entry = contraption[(new_point.r as usize, new_point.c as usize)];
+                if new_entry == '.' {
+                    // good, continue along
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                } else if new_entry == '|' {
+                    if (light_direction == North) | (light_direction == South) {
+                        light_position = new_point;
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    } else {
+                        light_position = new_point;
+                        light_direction = North;
+                        beam_list.push_back((new_point, South));
+                        beam_list.push_back((new_point, South));
+                        beam_list_done.push_back((new_point, South));
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    }
+                } else if new_entry == '-' {
+                    if (light_direction == East) | (light_direction == West) {
+                        light_position = new_point;
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    } else {
+                        light_position = new_point;
+                        light_direction = East;
+                        beam_list.push_back((new_point, West));
+                        beam_list_done.push_back((new_point, West));
+                        if !energised_vec.contains(&light_position) {
+                            energised_vec.push(light_position);
+                        }
+                    }
+                } else if new_entry == '\\' {
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                    if light_direction == North {
+                        light_direction = West;
+                    } else if light_direction == East {
+                        light_direction = South;
+                    } else if light_direction == South {
+                        light_direction = East;
+                    } else if light_direction == West {
+                        light_direction = North;
+                    }
+                } else if new_entry == '/' {
+                    light_position = new_point;
+                    if !energised_vec.contains(&light_position) {
+                        energised_vec.push(light_position);
+                    }
+                    if light_direction == North {
+                        light_direction = East;
+                    } else if light_direction == East {
+                        light_direction = North;
+                    } else if light_direction == South {
+                        light_direction = West;
+                    } else if light_direction == West {
+                        light_direction = South;
+                    }
+                }
+            }
+        }
+        configuration_energised.push(energised_vec.len());
+    }
+
+    println!("Part 2: {:?}", configuration_energised.iter().max().unwrap());
+    // 8236 too low?
+    // 8239 brute force is awesome!
+    // I know this would have been easy to parallelise, but maybe another day
+
+
 
     Ok(())
 }
